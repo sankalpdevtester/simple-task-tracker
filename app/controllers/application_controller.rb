ab68fac9699bcc62fa6d8c104b2e76ccacb0056b
sankalpdevtester <ohofufic0@gmail.com>
2026-06-28 18:28:15 +0530

    feat: add application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, unless: -> { controller_name == 'tasks' && action_name == 'index' }
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
