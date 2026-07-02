d72e94145b90cbecab6802db8674317ccfd8aab8
sankalpdevtester <ohofufic0@gmail.com>
2026-07-02 14:00:28 +0530

    feat: add login form component

---
+++ app/javascript/components/LoginForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('/sessions', { email, password })
+      .then((response) => {
+        localStorage.setItem('token', response.data.token);
+        window.location.href = '/';
+      })
+      .catch((error) => {
+        setError(error.response.data.error);
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </label>
+      <br />
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
