b2aeac43e99c46f5b409b833c446a3fc48c05c72
sankalpdevtester <ohofufic0@gmail.com>
2026-07-21 20:59:04 +0530

    feat: add task notification service to send reminders to users

--- app/services/task_notification_service.rb
+++ app/services/task_notification_service.rb
@@ -6,34 +6,23 @@ class TaskNotificationService
   end
 
   def send_reminder
-    # Check if the task has a reminder set
-    return unless @task.reminder_at
+    return unless @task.due_date && @task.user
 
-    # Calculate the time until the reminder
-    time_until_reminder = (@task.reminder_at - Time.current).to_i
+    # Calculate the time until the task is due
+    time_until_due = (@task.due_date - Time.current)
 
-    # Send a reminder email if the time until the reminder is less than 1 day
-    if time_until_reminder < 1.day
+    # Send a reminder if the task is due within the next 24 hours
+    if time_until_due <= 24.hours
       TaskMailer.reminder(@task).deliver_now
     end
   end
 
-  def send_update_notification
-    # Send a notification email when the task is updated
-    TaskMailer.update_notification(@task).deliver_now
-  end
-
-  def send_completion_notification
-    # Send a notification email when the task is completed
-    TaskMailer.completion_notification(@task).deliver_now
+  def self.send_daily_reminders
+    Task.where("due_date <= ?", Time.current + 1.day).each do |task|
+      TaskNotificationService.new(task).send_reminder
+    end
   end
 end
-
-# Usage example:
-# task = Task.find(1)
-# TaskNotificationService.new(task).send_reminder
-# TaskNotificationService.new(task).send_update_notification
-# TaskNotificationService.new(task).send_completion_notification
 ```
 
 ```ruby
@@ -41,17 +30,17 @@ end
 class TaskMailer < ApplicationMailer
   def reminder(task)
     @task = task
-    mail to: task.user.email, subject: "Reminder: #{@task.name}"
+    mail to: task.user.email, subject: "Task Reminder: #{@task.title}"
   end
+end
+```
 
-  def update_notification(task)
-    @task = task
-    mail to: task.user.email, subject: "Task updated: #{@task.name}"
-  end
-
-  def completion_notification(task)
-    @task = task
-    mail to: task.user.email, subject: "Task completed: #{@task.name}"
+```ruby
+# config/environments.rb (updated)
+Rails.application.configure do
+  # ...
+  config.after_initialize do
+    TaskNotificationService.send_daily_reminders
   end
 end
 ```
@@ -59,33 +48,16 @@ end
 ```ruby
 # app/controllers/tasks_controller.rb (updated)
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[update destroy]
+  # ...
+  def create
+    # ...
+    TaskNotificationService.new(@task).send_reminder
+    # ...
+  end
 
   def update
-    if @task.update(task_params)
-      TaskNotificationService.new(@task).send_update_notification
-      render json: @task, status: :ok
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    if @task.destroy
-      TaskNotificationService.new(@task).send_completion_notification
-      head :no_content
-    else
-      render json: @task.errors, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
-  def task_params
-    params.require(:task).permit(:name, :description, :reminder_at)
+    # ...
+    TaskNotificationService.new(@task).send_reminder
+    # ...
   end
 end
